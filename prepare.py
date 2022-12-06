91fe4965616a584b4ce5daf20a24a27855c942fb
Mark Foster <markdouglasfoster@gmail.com>
2022-12-06 10:08:24 -0600

    Add prep functions for store and ops data

---
+++ prepare.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from datetime import timedelta, datetime
+import numpy as np
+
+def acquire_and_prep_sales():
+    df = pd.read_csv('tsa_store_data.csv')
+    df['sale_date'] = pd.to_datetime(df.sale_date)
+    df = df.set_index('sale_date').sort_index()
+    df['month'] = df.index.month_name()
+    df['day_of_week'] = df.index.day_name()
+    
+    return df
+
+def acquire_and_prep_ops():
+    df = pd.read_csv('opsd_germany_daily.csv')
+    df['Date'] = pd.to_datetime(df.Date)
+    df = df.set_index('Date').sort_index()
+    df['Year'] = df.index.year
+    df['Month'] = df.index.month_name()
+    
+    return df
